b5d459141156f8791b2bf9ada844558499241adc
Cristian Rosales <cristianedrosales0207@gmail.com>
2025-10-13 00:23:15 -0300

    feat: agrega funciones de gestión de categorías al módulo de servicios

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,4 @@
+from .categories_service import create_category, get_categories, get_category
 from .user_service import create_user
 
-__all__ = ["create_user"]
+__all__ = ["create_user", "create_category", "get_category", "get_categories"]
