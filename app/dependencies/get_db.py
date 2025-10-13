bb9ec28c1e8cce4604aa5d189caf2e8f762ef848
Cristian Rosales <cristianedrosales0207@gmail.com>
2025-10-12 21:11:28 -0300

    feat: actualiza la gestión de sesiones de la base de datos a modo asíncrono

--- app/dependencies/get_db.py
+++ app/dependencies/get_db.py
@@ -1,12 +1,9 @@
 """dependencia para obtener la sesion de la DB en FastAPI"""
 
-from app.core import SessionLocal
+from app.core import async_session
 
 
-def get_db():
+async def get_db():
     """dependencias de FastAPI para obtener la sesion de la DB. Crea y cierra la sesion automaticamente"""
-    db = SessionLocal()
-    try:
+    async with async_session() as db:
         yield db
-    finally:
-        db.close()
