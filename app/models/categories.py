f0c9bea589c36a8effd3a8785e44045aaa8c57ae
Cristian Rosales <cristianedrosales0207@gmail.com>
2025-10-12 22:18:52 -0300

    feat: renombra la clase Category a CategoryDB para mayor claridad en el modelo de datos

--- app/models/categories.py
+++ app/models/categories.py
@@ -17,7 +17,7 @@ class CategoryOut(CategoryCreate):
     category_id: int
 
 
-class Category(CategoryCreate, table=True):
+class CategoryDB(CategoryCreate, table=True):
     """
     Representa una categoría de comida en la base de datos.
     Cada categoría tiene un identificador único y un nombre descriptivo.
