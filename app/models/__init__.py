16bfbe561c68ab891b20c5a776c14cd67cbb0368
Cristian Rosales <cristianedrosales0207@gmail.com>
2025-10-12 22:19:00 -0300

    feat: actualiza las importaciones y el __all__ en el módulo de modelos para incluir las clases de categorías

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,17 @@
-from .users import User  # noqa: F401
+from .categories import (  # noqa: F401
+    CategoriesResponse,
+    CategoryCreate,
+    CategoryDB,
+    CategoryOut,
+)
+from .users import UserBD, UserCreate, UserResponse  # noqa: F401
 
-__all__ = ["User"]
+__all__ = [
+    "UserCreate",
+    "UserResponse",
+    "UserBD",
+    "CategoryDB",
+    "CategoryCreate",
+    "CategoryOut",
+    "CategoriesResponse",
+]
