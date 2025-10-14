5dea948bd9aa6cd9b4dfe1759681caefb45cecfa
Cristian Rosales <cristianedrosales0207@gmail.com>
2025-10-14 19:41:17 -0300

    feat: agrega delete_category al módulo de servicios

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,15 @@
-from .categories_service import create_category, get_categories, get_category
+from .categories_service import (
+    create_category,
+    delete_category,
+    get_categories,
+    get_category,
+)
 from .user_service import create_user
 
-__all__ = ["create_user", "create_category", "get_category", "get_categories"]
+__all__ = [
+    "create_user",
+    "create_category",
+    "get_category",
+    "get_categories",
+    "delete_category",
+]
