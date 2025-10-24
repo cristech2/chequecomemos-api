d76cf9efa0a2d5bf5b2c3aa42d253a8bb1cce1ea
Cristian Rosales <cristianedrosales0207@gmail.com>
2025-10-24 20:02:30 -0300

    feat(models): agrega modelo y contratos para la tabla ingredients

---
+++ app/models/ingredients.py
@@ -0,0 +1,57 @@
+"""Modelo para la tabla ingredients y contrato para el cliente y la respuesta del servidor."""
+
+import uuid
+
+from sqlmodel import (
+    Field,  # pyright: ignore[reportUnknownVariableType]
+    Relationship,
+    SQLModel,
+)
+
+from .categories import Categories
+
+
+class IngredientsBase(SQLModel):
+    """Modelo base para los ingredientes de comida."""
+
+    # Nombre del ingrediente. Debe ser único y no nulo.
+    name: str
+    category_id: uuid.UUID
+    default_unit: str
+
+
+class Ingredients(IngredientsBase, table=True):
+    """Modelo de la tabla ingredients en la base de datos."""
+
+    ingredient_id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        primary_key=True,
+        unique=True,
+    )
+    category: "Categories" = Relationship(back_populates="ingredients")
+
+
+class IngredientCreate(IngredientsBase):
+    """Contrato para crear un nuevo ingrediente."""
+
+    pass
+
+
+class IngredientUpdate(SQLModel):
+    """
+    Modelo para actualizar un ingrediente existente.
+    Permite modificar solo el nombre y la unidad por defecto.
+    """
+
+    name: str
+    default_unit: str
+
+
+class IngredientResponse(IngredientsBase):
+    """
+    Modelo de respuesta para mostrar los datos completos de un ingrediente.
+    Incluye el id y la relación con la categoría.
+    """
+
+    id: uuid.UUID
+    category: Categories | None = None
